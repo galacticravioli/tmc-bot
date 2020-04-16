6f0062296e77d253bb55b2634e64a143bc0cb67b
galacticravioli <galacticravioli@gmail.com>
2020-04-16 20:37:51 +0200

    Add files via upload

--- main.rb
+++ main.rb
@@ -1,15 +1,20 @@
 require 'discordrb'
 require 'json'
 
-bot = Discordrb::Bot.new token: 'token lol'
+bot = Discordrb::Bot.new token: 'token'
 
 def playerjson
-  return File.read('Players.json')
+  json = File.read('Players.json')
+  return JSON.parse(json)
 end
 
+def saveplayerjson(playerhash)
+  File.open('players.json', 'w') do |f|
+    f.write(playerhash.to_json)
+  end
+end
 
-players = :readplayers
-puts players
+@players = playerjson
 
 bot.message(with_text: 'ping') do |event|
   event.respond 'Pong!'
@@ -17,25 +22,39 @@ end
 
 bot.message(with_text: '!go mining') do |event|
   i = rand(1..15)
+  @players[event.user.id.to_s] += i
+  saveplayerjson(@players)
   event.respond 'you found ' + i.to_s + ' diamonds!'
-  event.user.id
+end
 
+bot.message(with_text: '!query') do |event|
+  i = @players[event.user.id.to_s]
+ # puts @players
+ # puts event.user.id
+ # puts i.to_s
+  event.respond ('you have ' + i.to_s + " Diamonds")
 end
 
 bot.message(with_text: '!mineregister') do |event|
-  temphash = { event.user.id => 0 }
-  # puts(temphash.to_json)
-  #
-  if playerjson.include?(event.user.id.to_s) == false
-     File.open('players.json', 'w') {|f|
-       f.write(temphash.to_json)
-      event.respond 'player registered'
-     }
+  @players = playerjson
+  if !@players.include?(event.user.id.to_s)
+    @players[event.user.id] = 0
+    saveplayerjson(@players)
+    event.respond 'player registered'
 
   else
     event.respond 'you\'re already registered!'
-    puts "2"
- end
+  end
 end
+
+bot.message(with_text: '!reload') do |event|
+  if event.user.id.to_s == "273551864027676675"
+    @players = playerjson
+    event.respond 'Reloaded!'
+
+  else event.respond 'your not Ted'
+  end
+end
+
 
 bot.run
