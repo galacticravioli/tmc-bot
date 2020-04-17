276e354cde5e06c1c3ca4e2009d358f5b9ae3a32
galacticravioli <galacticravioli@gmail.com>
2020-04-17 21:32:54 +0200

    Add files via upload

--- main.rb
+++ main.rb
@@ -1,10 +1,11 @@
+
 require 'discordrb'
 require 'json'
 
 bot = Discordrb::Bot.new token: 'token'
 
 def playerjson
-  json = File.read('Players.json')
+  json = File.read('players.json')
   JSON.parse(json)
 end
 
@@ -15,6 +16,49 @@ def saveplayerjson(playerhash)
 end
 
 @players = playerjson
+
+def invjson
+  json = File.read('inventories.json')
+  JSON.parse(json)
+end
+
+def saveinvjson(invhash)
+  File.open('inventories.json', 'w') do |f|
+    f.write(invhash.to_json)
+  end
+end
+
+@inv = invjson
+
+def buy(name, item, price)
+  if @players[name] - price >= 0
+    @players[name] -= price
+    @inv[name] << item
+    saveinvjson(@inv)
+    saveplayerjson(@players)
+    return(true)
+  else
+    return(false)
+  end
+end
+
+def invregister(name)
+  @inv[name] = []
+  saveinvjson(@inv)
+end
+
+bot.message(with_text: '!store') do |event|
+  event.respond 'amulet of richness:1000d'
+end
+
+
+bot.message(with_text: '!buy amulet of richness') do |event|
+  if buy(event.user.id.to_s, 'amulet of richness', 1000)
+    event.respond 'bought'
+  else
+    event.respond 'you don\'t have enough money'
+  end
+end
 
 bot.message(with_text: 'ping') do |event|
   event.respond 'Pong!'
@@ -34,14 +78,13 @@ end
 
 bot.message(with_text: '!gamble') do |event|
   i = rand(-7..5)
-  x = ''
   @players[event.user.id.to_s] += i
-  if i == 0
+  if i.zero
     event.respond 'you broke even'
 
-    elsif i > 0 then event.respond 'you won ' + i.to_s + ' Diamonds'
+  elsif i > 0 then event.respond 'you won ' + i.to_s + ' Diamonds'
 
-  elsif i < 0 then event.respond 'you lost ' + (i*-1).to_s + ' Diamonds'
+  elsif i < 0 then event.respond 'you lost ' + (i * -1).to_s + ' Diamonds'
 
   end
   saveplayerjson(@players)
@@ -55,13 +98,35 @@ bot.message(with_text: '!query') do |event|
   event.respond ('you have ' + i.to_s + ' Diamonds')
 end
 
+bot.message(with_text: '!inv') do |event|
+  i = @inv[event.user.id.to_s]
+
+  event.respond ('you have: ' + i.join(", "))
+  end
+
+bot.message(with_text: 'name jeff') do |event|
+  event.respond 'no my name jeff'
+end
+
+bot.message(with_text: 'shut the fuck up') do |event|
+  event.respond 'no'
+end
+
+bot.message(with_text: 'sex') do |event|
+  event.respond 'kinky'
+end
+
+bot.message(with_text: 'anal suitcase') do |event|
+  event.respond 'good job hiding the drugs'
+end
+
 bot.message(with_text: '!register') do |event|
   @players = playerjson
   if !@players.include?(event.user.id.to_s)
     @players[event.user.id.to_s] = 0
     saveplayerjson(@players)
+    invregister(event.user.id.to_s)
     event.respond 'player registered'
-
   else
     event.respond 'you\'re already registered!'
   end
@@ -70,6 +135,7 @@ end
 bot.message(with_text: '!reload') do |event|
   if event.user.id.to_s == '273551864027676675'
     @players = playerjson
+    @inv = invjson
     event.respond 'Reloaded!'
 
   else event.respond 'your not Ted'
@@ -88,8 +154,19 @@ bot.message(with_text: 'sex') do |event|
   event.respond 'kinky'
 end
 
-bot.message(with_text: 'anal suitcase') do |event|
-  event.respond 'good job hiding the drugs'
+bot.message(with_text: '!covid test') do |event|
+  x = rand(1..2)
+  i = if x > 1
+        'positve'
+      else
+        'negative'
+      end
+
+  event.respond 'you tested ' + i + ' for covid!'
+end
+
+bot.message(with_text: 'creeper') do |event|
+  event.respond 'Awww man'
 end
 
 bot.run
